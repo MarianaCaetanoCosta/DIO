413e20bf458b3459e8723c2c13e7e353a4623438
Mariana Caetano <mariana.cae.costa@gmail.com>
2023-04-15 14:47:28 -0300

    #Aula6-Concluindo a criação das classes do dominio
    #Aula6-Concluindo a criação das classes do domínio
    - Classe Dev: criar regras de negócios para os métodos.

--- desafio-dio-poo-java/src/br/com/dio/desafio/dominio/Dev.java
+++ desafio-dio-poo-java/src/br/com/dio/desafio/dominio/Dev.java
@@ -2,6 +2,7 @@ package br.com.dio.desafio.dominio;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev{
@@ -12,11 +13,36 @@ public class Dev{
     private Set<Conteudo> conteudosConcluidos= new LinkedHashSet<>();
 
     //Métodos
-    public void inscreverBootcamp(Bootcamp bootcamp){}
+    public void inscreverBootcamp(Bootcamp bootcamp){
+        //Inscrito no bootcamp deve ter todos os conteudos.
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
 
-    public void progradir(){}
+        //Pegar o bootcamp e adicionar o DEV, a palavra this pega o argumento passado pelo método
+        bootcamp.getDevsInscritos().add(this);
+    }
 
-    public void calcularTotalXp(){}
+    public void progradir(){
+        //Pegar o conteudo na ordem em que foi adicionado
+        //Optional: resolve retornos nulos
+         Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+
+         //Pegar e colocar em conteudos inscritos
+        if (conteudo.isPresent()){
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        }
+        else{
+            System.err.println("Você não está matriculado em nenhum contéudo!");
+        }
+    }
+
+    public double calcularTotalXp(){
+        //ConteudosIncluidos pegar o xp de cada conteúdo incluido e somar
+        return this.conteudosConcluidos
+                .stream()
+                .mapToDouble(Conteudo::calculoXp)
+                .sum();
+    }
 
 
     /* Get/Set */
